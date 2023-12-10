a6ac0524238e901876849506d3719c5bd9541401
HawkClaws <laspid07@gmail.com>
2023-12-10 15:41:41 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version="0.0.1",
     author="HawkClaws",
     packages=find_packages(),
-    install_requires=["trafilatura","html2text","beautifulsoup4"],
+    install_requires=[
+        "trafilatura>=1.6.2",
+        "html2text>=2020.1.16",
+        "beautifulsoup4>=4.12.2",
+    ],
+    python_requires=">=3.6",
     include_package_data=True,
-    url=""
+    url="",
+    license="MIT",
 )
